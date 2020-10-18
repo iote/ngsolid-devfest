9fc6bed0dbb6a95fb2485e4ec611de71a70f25c2
Jente Rosseel <rosseeljente@gmail.com>
2020-10-18 08:25:15 +0200

    initial commit

---
+++ apps/ngsolid-devfest/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngsolid-devfest-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ngsolid-devfest';
+}
---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  projects: ['<rootDir>/apps/ngsolid-devfest'],
+};
